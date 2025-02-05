75210b504f788b468cf1c0f68d7476463b37fd4e
Emma Acquah <emmaacquah@gmail.com>
2025-02-05 21:19:39 +0000

    Add HashMap to track vehicle details.

--- CarParkPriorityQueue.java
+++ CarParkPriorityQueue.java
@@ -1,7 +1,9 @@
+import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class CarParkPriorityQueue {
 
+//    private PriorityQueue<Integer> availableParkingSlots;
     private PriorityQueue<Integer> availableParkingSlots;
     private int capacity;
     private HashMap<Integer, String> parkedCars;
@@ -9,8 +11,7 @@ public class CarParkPriorityQueue {
     public CarParkPriorityQueue(int capacity) {
         this.availableParkingSlots = new PriorityQueue<>(capacity);
         this.capacity = capacity;
-//        this.parkedCars = new HashMap<>(capacity); // does that init work?
-        this.parkedCars = new HashMap<>(); // does that init work?
+        this.parkedCars = new HashMap<>();
 
         for (int i = 1; i <= capacity; i++) {
             availableParkingSlots.add(i);
@@ -21,19 +22,25 @@ public class CarParkPriorityQueue {
         return availableParkingSlots;
     }
 
-    public Integer parkCar() throws Exception {
+    public Integer parkCar(String licensePlateNumber) throws Exception {
         if (availableParkingSlots.isEmpty()) {
             throw new Exception("No available parking slots. Car not parked.");
         } else {
-            return availableParkingSlots.poll();
+            Integer parkingSlotNumber = availableParkingSlots.poll();
+            parkedCars.put(parkingSlotNumber, licensePlateNumber);
+            return parkingSlotNumber;
+
         }
     }
 
-    public void freeParkingSlot(Integer parkingSlotNumber) throws Exception {
+    public void freeParkingSlot(Integer parkingSlotNumber, String licensePlateNumber) throws Exception {
         if (parkingSlotNumber > capacity || parkingSlotNumber <= 0) {
             throw new Exception("Invalid parking slot number.");
+        } else if (!parkedCars.get(parkingSlotNumber).equals(licensePlateNumber)) {
+            throw new Exception("Invalid license plate number");
         } else {
             availableParkingSlots.add(parkingSlotNumber);
+            parkedCars.remove(parkingSlotNumber);
         }
     }
 
