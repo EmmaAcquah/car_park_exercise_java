a77f48ae3eeb613e6347acecc14a5f55059b9837
Emma Acquah <emmaacquah@gmail.com>
2025-02-07 00:48:45 +0000

    Test adding reservations

--- CarParkPriorityQueue.java
+++ CarParkPriorityQueue.java
@@ -7,33 +7,46 @@ public class CarParkPriorityQueue {
     private PriorityQueue<Integer> availableParkingSlots;
     private int capacity;
     private HashMap<Integer, String> parkedCars;
+//    private HashMap<Integer, String> reservedParkingSlots;
+    private HashMap<String, Integer> reservedParkingSlots;
 
     public CarParkPriorityQueue(int capacity) {
         this.availableParkingSlots = new PriorityQueue<>(capacity);
         this.capacity = capacity;
         this.parkedCars = new HashMap<>();
+        this.reservedParkingSlots = new HashMap<>();
 
         for (int i = 1; i <= capacity; i++) {
             availableParkingSlots.add(i);
         }
     }
 
-    public PriorityQueue<Integer> checkAvailableParkingSlots() {
-        return availableParkingSlots;
     public HashMap<Integer, String> getParkedCars() {
         return parkedCars;
     }
 
+    public PriorityQueue<Integer> checkAvailableParkingSlots() throws Exception{
+        if (availableParkingSlots.isEmpty()) {
+            throw new Exception("No available parking slots");
+        } else {
+            return availableParkingSlots;
+        }
+//        return availableParkingSlots;
     }
 
     public Integer parkCar(String licensePlateNumber) throws Exception {
         if (availableParkingSlots.isEmpty()) {
-            throw new Exception("No available parking slots. Car not parked.");
+            throw new Exception("No available parking slots.");
+//        } else if (reservedParkingSlots.containsValue(licensePlateNumber)) {
+        } else if (reservedParkingSlots.containsKey(licensePlateNumber)) {
+            Integer parkingSlotNumber = reservedParkingSlots.get(licensePlateNumber);
+            parkedCars.put(parkingSlotNumber, licensePlateNumber);
+            reservedParkingSlots.remove(licensePlateNumber);
+            return parkingSlotNumber;
         } else {
             Integer parkingSlotNumber = availableParkingSlots.poll();
             parkedCars.put(parkingSlotNumber, licensePlateNumber);
             return parkingSlotNumber;
-
         }
     }
 
@@ -46,6 +59,18 @@ public class CarParkPriorityQueue {
             availableParkingSlots.add(parkingSlotNumber);
             parkedCars.remove(parkingSlotNumber);
         }
+    }
+
+    public Integer reserveParkingSlot(String licensePlateNumber) throws Exception {
+        if (availableParkingSlots.isEmpty()) {
+            throw new Exception("No available parking slots.");
+        }
+
+        Integer nearestParkingSlot = availableParkingSlots.poll();
+//        reservedParkingSlots.put(nearestParkingSlot, licensePlateNumber);
+        reservedParkingSlots.put(licensePlateNumber, nearestParkingSlot);
+
+        return nearestParkingSlot;
     }
 
 }
